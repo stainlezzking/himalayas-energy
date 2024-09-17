1c83d48e30c3e9447cbf993cf3772c454d31e113
Chukwuebuka Stanley <azukachukwuebuka07@gmail.com>
2024-09-17 11:04:26 +0100

    completed the service page responsiveness

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -24,9 +24,9 @@ const Page = () => {
           </span>
           <div className="relative gap-x-6 items-center">
             <span className="bg-primary block absolute top-1/2 -translate-y-1/2 w-2 h-[100px]"></span>
-            <div className="space-y-3 ps-6 w-[630px] ">
+            <div className="space-y-3 ps-6 w-[630px] max-w-full">
               <h4 className="text-main text-[14px]">WHO WE ARE</h4>
-              <h1 className="text-4xl font-bold">Innovating for a Sustainable Energy Future in Africa</h1>
+              <h1 className="text-2xl sm:text-4xl font-bold">Innovating for a Sustainable Energy Future in Africa</h1>
             </div>
           </div>
         </div>
@@ -41,7 +41,7 @@ const Page = () => {
               <circle cx="4.5" cy="3.5" r="3" transform="rotate(-90 4.5 3.5)" fill="#181818" stroke="#D9D9D9" />
             </svg>
           </span>
-          <div className="pageMargin grid grid-cols-4 gap-x-5 my-[94px]">
+          <div className="pageMargin grid grid-cols-2 md:grid-cols-4 gap-5 my-[94px]">
             <div className="col-span-1 aspect-square relative">
               <Image
                 src={service1}
@@ -83,11 +83,11 @@ const Page = () => {
               <span className="text-2xl pb-1 border-b border-b-primary">01</span>
               <h1 className="font-bold">ONSHORE & OFFSHORE PIPELINE INSTALLATION</h1>
               <p className="text-body1">
-                Himalayas Energy can supply a wide range of services for Onshore/Offshore projects, covering the entire project life span, from the
-                earliest conceptual studies, Basics, FEED to Detailed Design. <br />
-                Himalayas’s subsea construction and installation services offer field development support to oil and gas companies. A combination of
-                our assets and engineering services enable our team to complete projects safely and on-time and within budget. Our Onshore/Offshore
-                Construction Services include:
+                Himalayas Energy can supply a wide range of services for Onshore/Offshore projects, covering the entire project life span, from the earliest
+                conceptual studies, Basics, FEED to Detailed Design. <br />
+                Himalayas’s subsea construction and installation services offer field development support to oil and gas companies. A combination of our assets
+                and engineering services enable our team to complete projects safely and on-time and within budget. Our Onshore/Offshore Construction Services
+                include:
               </p>
               <span className="flex gap-x-2 items-center text-body1">
                 <svg width="13" height="14" viewBox="0 0 13 14" fill="none" className="block shrink-0">
@@ -148,11 +148,11 @@ const Page = () => {
               <span className="text-2xl pb-1 border-b border-b-primary">02</span>
               <h1 className="font-bold">LOCAL GAS DISTRIBUTION</h1>
               <p className="text-body1">
-                Himalayas Energy builds pipeline transportation networks. We provide value to our customers and the industry by constructing pipeline
-                networks to connect producing wells and treatment facilities to onshore or offshore terminals for separation, treating, storage, and
-                off-take. Himalayas Energy’s pipeline technicians are highly skilled operators of offshore installation vessels with extensive
-                experience constructing small diameter to large diameter pipeline systems and networks. Our experience spans from areas near shore, in
-                shallow water, or between shore and a connection point in deeper water. <br />
+                Himalayas Energy builds pipeline transportation networks. We provide value to our customers and the industry by constructing pipeline networks
+                to connect producing wells and treatment facilities to onshore or offshore terminals for separation, treating, storage, and off-take. Himalayas
+                Energy’s pipeline technicians are highly skilled operators of offshore installation vessels with extensive experience constructing small
+                diameter to large diameter pipeline systems and networks. Our experience spans from areas near shore, in shallow water, or between shore and a
+                connection point in deeper water. <br />
                 Himalayas Energy’s pipeline IRM work includes:
               </p>
               <span className="flex gap-x-2 items-center text-body1">
@@ -196,8 +196,8 @@ const Page = () => {
               <span className="text-2xl pb-1 border-b border-b-primary">03</span>
               <h1 className="font-bold">ENVIRONMENTAL / WASTE MANAGEMENT</h1>
               <p className="text-body1">
-                We offer cost effective waste management solutions across multiple industries from medium to large scale in the upstream and
-                downstream oil and gas sector.
+                We offer cost effective waste management solutions across multiple industries from medium to large scale in the upstream and downstream oil and
+                gas sector.
               </p>
               <span className="flex gap-x-2 items-center text-body1">
                 <svg width="13" height="14" viewBox="0 0 13 14" fill="none" className="block shrink-0">
@@ -251,32 +251,31 @@ const Page = () => {
               <span className="text-2xl pb-1 border-b border-b-primary">05</span>
               <h1 className="font-bold">MANPOWER SERVICES</h1>
               <p className="text-body1">
-                Himalayas Energy is a recruitment specialist for employers and professionals in the oil, gas and energy sector. We provide contract
-                and permanent recruitment opportunities across Nigeria and West Africa. We have specialist discipline knowledge for the people who are
-                responsible for Engineering Construction, Repairs, Modification and Maintenance, Project Management, Subsurface, Subsea and Marine,
-                HSEQ, Waste Management , Petroleum Product Haulage and Seismic Surveys. <br />
-                Himalayas Energy also recruits and mobilises business service and operational experts for Supply Chain, IT, Finance, HR, Sales and
-                Manufacturing opportunities. We speak the language of the oil, gas and energy industry and have a genuine understanding of client
-                requirements. <br />
+                Himalayas Energy is a recruitment specialist for employers and professionals in the oil, gas and energy sector. We provide contract and
+                permanent recruitment opportunities across Nigeria and West Africa. We have specialist discipline knowledge for the people who are responsible
+                for Engineering Construction, Repairs, Modification and Maintenance, Project Management, Subsurface, Subsea and Marine, HSEQ, Waste Management ,
+                Petroleum Product Haulage and Seismic Surveys. <br />
+                Himalayas Energy also recruits and mobilises business service and operational experts for Supply Chain, IT, Finance, HR, Sales and Manufacturing
+                opportunities. We speak the language of the oil, gas and energy industry and have a genuine understanding of client requirements. <br />
                 The flexibility of our business model means our clients can outsource their recruitment function entirely or complement their internal
-                capabilities with our services. Our network of skilled professionals is constantly evolving and expanding throughour innovative use of
-                social media, online advertising and database technology building. <br />
-                We build engagement with thousands of potential candidates – and organically via referrals and recommendations from existing clients
-                and candidates.
+                capabilities with our services. Our network of skilled professionals is constantly evolving and expanding throughour innovative use of social
+                media, online advertising and database technology building. <br />
+                We build engagement with thousands of potential candidates – and organically via referrals and recommendations from existing clients and
+                candidates.
               </p>
             </li>
             <li className="space-y-3">
               <span className="text-2xl pb-1 border-b border-b-primary">06</span>
               <h1 className="font-bold">OIL & GAS EXPLORATION AND FIELD DEVELOPMENT PROGRAM</h1>
               <p className="text-body1">
-                Himalayas Energy Consults Limited has expertise to optimize onshore and offshore field development, reduce Capex and expedite your
-                production start. Our Onshore and Offshore Field Development Programs include:
+                Himalayas Energy Consults Limited has expertise to optimize onshore and offshore field development, reduce Capex and expedite your production
+                start. Our Onshore and Offshore Field Development Programs include:
               </p>
-              <span className="flex gap-x-2 items-center text-body1">
+              <span className="flex flex-wrap gap-x-2 items-center text-body1">
                 <svg width="13" height="14" viewBox="0 0 13 14" fill="none" className="block shrink-0">
                   <circle cx="6.5" cy="7" r="4.5" fill="white" stroke="#C00000" strokeWidth="4" />
                 </svg>
-                <span className="font-medium text-main">Oil Field Assessment:</span> Assessing the volume of oil and gas in the reservoir.
+                <span className="font-medium text-main shrink-0">Oil Field Assessment:</span> Assessing the volume of oil and gas in the reservoir.
               </span>
               <span className="flex gap-x-2 items-center text-body1">
                 <svg width="13" height="14" viewBox="0 0 13 14" fill="none" className="block shrink-0">
@@ -333,8 +332,8 @@ const Page = () => {
                 ENGINEERING CONSTRUCTION / OPERATION / MODIFICATTON / REPAIR / MAINTENANCE SERVICES FOR SITE / PLATFORMS / PROPERTIES
               </h1>
               <p className="text-body1">
-                geting the offshore & on shore industry, Himalayas Energy provides a wide range of services in operation, modification, repair and
-                maintenance services for offshore and onshore site and platforms. Areas we cover are listed as follows:
+                geting the offshore & on shore industry, Himalayas Energy provides a wide range of services in operation, modification, repair and maintenance
+                services for offshore and onshore site and platforms. Areas we cover are listed as follows:
               </p>
               <span className="flex gap-x-2 items-center text-body1">
                 <svg width="13" height="14" viewBox="0 0 13 14" fill="none" className="block shrink-0">
